ac2244dc9bb23c787624632a6c049faed9f415e0
674193357@qq.com <qq123456>
2018-05-14 20:46:15 +0800

    Initial commit

---
+++ app/src/main/java/com/chen/material/materialcomponentsdemo/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.chen.material.materialcomponentsdemo.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
