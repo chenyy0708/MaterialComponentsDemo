5ede9ce03aa182d061ea74ec33a5dce2deaf4dd0
674193357@qq.com <qq123456>
2018-05-15 20:54:22 +0800

    BottomAppBar菜单、点击事件、动画

--- app/src/main/java/com/chen/material/materialcomponentsdemo/BottomAppBarActivity.java
+++ app/src/main/java/com/chen/material/materialcomponentsdemo/BottomAppBarActivity.java
@@ -1,14 +1,55 @@
 package com.chen.material.materialcomponentsdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class BottomAppBarActivity extends AppCompatActivity {
+
+    private BottomAppBar bottomAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_app_bar);
+
+        bottomAppBar = findViewById(R.id.bar);
+
+        findViewById(R.id.bt_left).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomAppBar.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_CENTER);
+            }
+        });
+        findViewById(R.id.bt_right).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomAppBar.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_END);
+            }
+        });
+        bottomAppBar.replaceMenu(R.menu.menu_main_nav);
+
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(bottomAppBar,"菜单",Snackbar.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Snackbar.make(bottomAppBar,"菜单",Snackbar.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
